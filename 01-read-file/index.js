8df4776eb4902b871cc52f3b0dd1e372d46deb4b
MarynaDanilkina <Danilkinamari99@gmail.com>
2022-05-14 21:32:29 +0300

    feat: add 01-read-file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,9 +1,6 @@
 const path = require('path');
-//Получим адрес файла text.txt
 const address = path.join(__dirname, 'text.txt');
 const fs = require('fs');
-//считать информацию
 let info = fs.createReadStream(address);
-//вывод через консоль
 info.on('data', text => console.log(text.toString()));
 // или info.on('data', text => console.log(`${text}`));
